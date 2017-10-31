1b9c4a2c1eac558361bace13503ce65fc1946d82
Alunno <Alunno@PC-25-INF4>
2017-10-31 12:55:18 +0100

    Prova

--- ProgettoTPS/src/View/Finestra.java
+++ ProgettoTPS/src/View/Finestra.java
@@ -1,21 +1,55 @@
 package View;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JList;
 
 public class Finestra {
 
-	private JFrame frame;
+	private JFrame frmQuizzone;
+	private JLabel lblDomanda;
+	private JLabel lblQuesito;
+	private JButton btnRispTrue;
+	private JButton btnRispFalse;
+	private JScrollPane scrollPane;
+	private JList list;
 	public Finestra() {
 		initialize();
 	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
+	
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmQuizzone = new JFrame();
+		frmQuizzone.setTitle("QUIZZONE");
+		frmQuizzone.setBounds(100, 100, 600, 400);
+		frmQuizzone.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmQuizzone.getContentPane().setLayout(null);
+		
+		lblDomanda = new JLabel("DOMANDA");
+		lblDomanda.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDomanda.setBounds(234, 11, 109, 14);
+		frmQuizzone.getContentPane().add(lblDomanda);
+		
+		lblQuesito = new JLabel("");
+		lblQuesito.setHorizontalAlignment(SwingConstants.CENTER);
+		lblQuesito.setBounds(10, 36, 564, 28);
+		frmQuizzone.getContentPane().add(lblQuesito);
+		
+		btnRispTrue = new JButton("New button");
+		btnRispTrue.setBounds(40, 278, 195, 72);
+		frmQuizzone.getContentPane().add(btnRispTrue);
+		
+		btnRispFalse = new JButton("New button");
+		btnRispFalse.setBounds(339, 278, 195, 72);
+		frmQuizzone.getContentPane().add(btnRispFalse);
+		
+		scrollPane = new JScrollPane();
+		scrollPane.setBounds(40, 38, 494, 229);
+		frmQuizzone.getContentPane().add(scrollPane);
+		
+		list = new JList();
+		scrollPane.setViewportView(list);
 	}
-
 }
