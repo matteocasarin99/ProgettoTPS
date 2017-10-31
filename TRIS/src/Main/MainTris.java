4dda5d6e97ec9c7e9494fc1c1c4c83ec0a8a62d0
Alunno <Alunno@PC-25-INF4>
2017-10-31 11:54:38 +0100

    Aggiunta file

---
+++ TRIS/src/Main/MainTris.java
@@ -0,0 +1,16 @@
+package Main;
+
+import Control.Controller;
+import Model.Gestione;
+import View.Finestra;
+
+public class MainTris {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Finestra f=new Finestra();
+		Gestione g=new Gestione();
+		Controller c=new Controller();
+	}
+
+}
---
+++ TRIS/src/View/Finestra.java
@@ -0,0 +1,21 @@
+package View;
+
+import javax.swing.JFrame;
+
+public class Finestra {
+
+	private JFrame frame;
+	public Finestra() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
