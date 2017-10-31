75d12011a2709031f95fc805688f4edf160b9fc0
Alunno <Alunno@PC-25-INF4>
2017-10-31 12:42:47 +0100

    Nuove classi e inizio formattazione classi server

---
+++ TRIS/src/Main/MainServer.java
@@ -0,0 +1,15 @@
+package Main;
+
+import Control.ControllerServer;
+import Model.GestioneServer;
+
+public class MainServer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//FinestraServer f=new FinestraServer();
+		GestioneServer g=new GestioneServer();
+		//ControllerServer c=new ControllerServer();
+	}
+
+}
